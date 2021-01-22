7749063959095033317be5a768baa82554ec2a84
Vishnu Divakar <vishnudivakar31@gmail.com>
2021-01-21 19:39:59 -0500

    rest API with transaction pool

--- src/main/java/io/vdev/application/Application.java
+++ src/main/java/io/vdev/application/Application.java
@@ -1,8 +1,7 @@
 package io.vdev.application;
 
 import io.vdev.dapp.BlockNode;
-import io.vdev.dapp.Transaction;
-import io.vdev.dapp.Wallet;
+import io.vdev.dapp.RestServer;
 
 import java.io.IOException;
 import java.security.InvalidKeyException;
@@ -13,26 +12,19 @@ import java.security.spec.InvalidKeySpecException;
 public class Application {
     public static void main(String[] args)
             throws IOException, NoSuchAlgorithmException, InterruptedException, SignatureException, InvalidKeyException, InvalidKeySpecException {
-        if(args.length != 1) {
-            System.out.println("**Error -- Expected one argument: port of the node");
+        if(args.length != 2) {
+            System.out.println("**Error -- Expected two arguments: port of the node -- port of the rest server");
             return;
         }
         int nodePort = Integer.parseInt(args[0]);
+        int serverPort = Integer.parseInt(args[1]);
         BlockNode blockNode = BlockNode.getInstance(nodePort);
-        if(blockNode.getCurrentIPPort().equals(blockNode.getKnownIpPort())) {
-            Wallet bobWallet = Wallet.getInstance();
-            Transaction bobTransaction = blockNode.getWallet().createTransaction(bobWallet.getPublicKey(), "TRANSFER", 10.2);
-            Transaction aliceTransaction = blockNode.getWallet().createTransaction(bobWallet.getPublicKey(), "TRANSFER", 11.2);
-            blockNode.addToTransactionPool(bobTransaction);
-            blockNode.addToTransactionPool(aliceTransaction);
-        }
+        RestServer server = RestServer.getInstance(blockNode);
+        server.start(serverPort);
         while (true) {
             Thread.sleep(10000);
             System.out.println("Known peers");
             System.out.println(blockNode.getPeers());
-            System.out.println("-------------------------------------");
-            System.out.println("Transaction Pool -- Size: " + blockNode.getPool().getPoolSize());
-            System.out.println(blockNode.getPool());
         }
     }
 }
---
+++ src/main/java/io/vdev/dapp/RestServer.java
@@ -0,0 +1,66 @@
+package io.vdev.dapp;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.javalin.Javalin;
+import io.javalin.http.Context;
+
+import java.io.IOException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SignatureException;
+import java.security.spec.InvalidKeySpecException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class RestServer {
+    private static RestServer instance = null;
+    private BlockNode blockNode;
+    private Javalin server;
+    private ObjectMapper objectMapper;
+
+
+    private RestServer(BlockNode blockNode) {
+        this.blockNode = blockNode;
+        this.objectMapper = new ObjectMapper();
+    }
+
+    public static RestServer getInstance(BlockNode blockNode) {
+        if(instance == null) {
+            instance = new RestServer(blockNode);
+        }
+        return instance;
+    }
+
+    public void start(int port) {
+        server = Javalin.create().start(port);
+        server.get("public_key", ctx -> fetchPublicKey(ctx));
+        server.get("/transaction_pool", ctx -> fetchTransactionPool(ctx));
+        server.post("/transaction", ctx -> createTransaction(ctx));
+    }
+
+    private void fetchPublicKey(Context ctx) throws JsonProcessingException {
+        Map<String, String> result = new HashMap<>();
+        result.put("public_key", blockNode.getWallet().getPublicKey());
+        ctx.result(objectMapper.writeValueAsString(result));
+    }
+
+    private void createTransaction(Context ctx)
+            throws IOException, NoSuchAlgorithmException, InvalidKeyException, SignatureException, InvalidKeySpecException {
+        String postBody = ctx.body();
+        Map<String, Object> body = objectMapper.readValue(postBody, Map.class);
+        Transaction transaction = blockNode.getWallet().createTransaction((String) body.get("receiver_public_key"),
+                (String )body.get("transaction_type"), (Double) body.get("amount"));
+        blockNode.addToTransactionPool(transaction);
+        ctx.status(201);
+        ctx.result(objectMapper.writeValueAsString(transaction));
+    }
+
+    private void fetchTransactionPool(Context ctx) throws JsonProcessingException {
+        List<Transaction> pool = blockNode.getPool().getTransactions();
+        ctx.result(objectMapper.writeValueAsString(pool));
+    }
+
+
+}
